6278115ea70005d670a36a1f4c37f4e694ae612f
Marc Retzlaff <retzlaff@hs-pforzheim.de>
2019-10-03 12:01:44 +0200

    Full throttle

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
+#include "Punkt.h"
+#include "Linie.h"
+#include "Grafik.h"
+
+#include "Rechteck.h"
+#include "Kreis.h"
+
 int main()
 {
+/* Samples */
+Punkt a(2.0,2.0);
+Punkt b(2.0,4.0);
+Punkt c(4.0,2.0);
+Punkt d(4.0,4.0);
+Kreis sK(a,3.0);
+Punkt punkt1(3.0,4.0);
+Rechteck quadrat(a,b,c,d);
+punkt1.Zeichne();
+Linie linie1(a,punkt1);
+linie1.Zeichne();
+sK.Zeichne();
+quadrat.Zeichne();
+
 return 0;
 }
